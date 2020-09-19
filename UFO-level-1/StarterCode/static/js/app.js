05a46dee73e0a2fcb10740042dec3e7c3c774ef0
rew0809 <65917215+rew0809@users.noreply.github.com>
2020-09-19 12:07:00 -0500

    Final Version

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -6,7 +6,7 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Step 1: Loop Through `data` and console.log each UFO sighting object and use d3 to append 1 cell per sighting to the table
+// Step 1: Loop Through `data` UFO sighting object and use d3 to append 1 cell per sighting to the table
 data.forEach(function(ufoSightings) {
     var row = tbody.append("tr");
     
@@ -33,6 +33,12 @@ function runEnter() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
+    //Clear Table
+    var table = document.getElementById("ufo-table");
+
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
+    }
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
     // Get the value property of the input element
@@ -40,4 +46,16 @@ function runEnter() {
     // Use the form input to filter the data by blood type
     var result = tableData.filter(tableData => tableData.datetime === inputValue);
     console.log(result);
+
+    
+    //Append Filtered Data
+    result.forEach(function(ufoSightings) {
+        var row = tbody.append("tr");
+        
+        Object.entries(ufoSightings).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
 }
